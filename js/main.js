f5b912e25afe5e66cb8d914bafef624da37cde5f
Gabriel López <gabi1517@hotmail.com>
2021-09-01 16:16:21 -0300

    desafio 8

--- js/main.js
+++ js/main.js
@@ -9,34 +9,33 @@ class Task {
 
 const tasks = [];
 
+let acumulator = ``;
+
 let addTask = () => {
-    let taskQuestion = prompt("¿Desea agregar una tarea? si/no");
-    while (taskQuestion == "si") {
-        let name = prompt("¿Cuál es el nombre de la tarea?");
-        let date = prompt("¿Cuál es la fecha de la tarea?");
-        let difficulty = prompt("Cuál es la dificultad de la tarea? facil/normal/dificil");
-        let priority = parseInt(prompt("¿Cuál es la prioridad de la tarea? 1/10"));
-        tasks.push(new Task(name, date, difficulty, priority));
-        taskQuestion = prompt("¿Desea agregar otra tarea? si/no");
-    };
+    let name = document.getElementById("name").value;
+    let date = document.getElementById("date").value;
+    let difficulty = document.getElementById("difficulty").value;
+    let priority = document.getElementById("priority").value;
+    tasks.push(new Task(name, date, difficulty, priority));
     tasks.sort(function (a, b) {
         return (b.priority - a.priority)
     });
-    tasks.forEach(element =>
-        document.write(`
-        <div class="${element.difficulty}Card">
-            <h3> ${element.name}<h3>
-            <h4>Fecha: ${element.date}</h4>
-            <h4>Dificultad: ${element.difficulty}<h4>
-            <h4>Prioridad: ${element.priority}<h4>
-        </div>
-        `
-        )
-    )
+    acumulator += `
+    <div class="${difficulty}Card">
+        <h3> ${name}<h3>
+        <h4>Fecha: ${date}</h4>
+        <h4>Dificultad: ${difficulty}<h4> 
+        <h4>Prioridad: ${priority}<h4>
+    </div>
+    `
+    document.getElementById("taskList").innerHTML = acumulator; 
 }
 
-addTask();
-
+//TODO: agregar un contador de número de tareas totales. al lado un número de tareas importantes.
+//TODO: hacer que se imprima la dificultad en español. 
+//TODO: agregar un botón de eliminar tarea en la card.
+//TODO: agregar botón de limpiar formulario de tarea nueva. 
+//TODO: cambiar número de prioridad por un botón que suba o baje la tarea en el listado, este botón va a cambiar la posición de la tarea en el array de tareas.
 
 
 
