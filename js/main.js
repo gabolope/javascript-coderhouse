d4498fbd49bd7dee0d1a666fbd186b663c6d84fc
Gabriel López <gabi1517@hotmail.com>
2021-09-30 17:18:51 -0300

    preparando para la entrega final

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 class Task {
-    constructor(name, date, difficulty, priority) {
+    constructor(name, difficulty, priority) {
         this.name = name;
-        this.date = date;
         this.difficulty = difficulty;
         this.priority = priority;
     };
 };
 
+let tasks = [];
 
-//Identificador de usuario
-let indentifyUser = () => {
+//Identifing user
+const indentifyUser = () => {
     if (localStorage.userName == null) {
         document.getElementById("userArea").innerHTML = `
         <form>
-            <div>Usted no se encuentra registrado</div>
-            <p>Por favor ingrese su nombre</p>
+            <div>No estás registrado</div>
+            <p>Por favor ingresá tu nombre</p>
             <input type="text" class="form-control mb-3" id="userName">
             <button onclick="addUser()" class="btn btn-primary mb-3">Registrarme</button>
         </form>
@@ -25,13 +25,13 @@ let indentifyUser = () => {
 }
 indentifyUser();
 
-let addUser = () => {
+const addUser = () => {
     let userName = document.getElementById("userName").value;
-    console.log(userName);
-    localStorage.setItem("userName", userName);
+    const userNameCapitalized = userName.charAt(0).toUpperCase() + userName.slice(1)
+    localStorage.setItem("userName", userNameCapitalized);
 }
 
-//Saludadador según horario del día (fecha tomada de sistema del usuario)
+//Greeting user according sistem date
 function checkTime(i) {
     if (i < 10) {
         i = "0" + i;
@@ -56,48 +56,40 @@ function greetUser() {
     }
 }
 
-//Selector de frases aleatorias
+//Displaying random phrase
 randomPhrase = () => {
     phrase = phrases[Math.floor(Math.random() * phrases.length)];
     document.getElementById("phrase").innerHTML = phrase;
 }
 randomPhrase()
 
-//Impresor de tareas
-let tasksPrinter = (tasksPrint) => {
+//Task Printer
+const tasksPrinter = (tasksPrint) => {
     let acumulator = ``;
     tasksPrint.forEach(task =>
         acumulator +=
         `
-            <div id="${task.name}" class="card-body mb-4 col-4 taskCard">
-                <div class="small text-muted">Fecha: ${task.date}</div> 
+            <div id="${task.name}" class="card-body mb-4 col-4 taskCard ${task.difficulty}">
                 <h2 class="card-title h4">${task.name}</h2>
-                <p class="card-text">Dificultad: ${task.difficulty}</p> 
+                <div class="card-text">Dificultad: ${task.difficulty}</div> 
                 <p class="card-text">Prioridad: ${task.priority}</p>
                 <button id="deleteButton" onclick="deleteTask('${task.name}')" class="btn btn-primary">Borrar</button>
             </div>
         `
         //TODO: validar que el usuario escriba un nombre 
-        //TODO: cambiar el nombre de la dificultad a español para mostrarlo al usuario, y cambiar el color de la fuente según la dificultad   
     )
     document.getElementById("taskList").innerHTML = acumulator;
     $(`.taskCard`).fadeIn(500);
 }
-let tasks = [];
 
-//Guardador de tareas en JSON
-let checkStorageTasks = () => {
+
+//Checking localStorage for saved user tasks
+const checkStorageTasks = () => {
     let storagedTasks = JSON.parse(localStorage.getItem("userTasks"));
     if (storagedTasks == null) {
-        console.log("no hay tareas");
-        document.getElementById("taskList").innerHTML = `Aún no se han añadido tareas`; //TODO: mejorar el formato estético
-        let name = document.getElementById("name").value;
-        let date = document.getElementById("date").value;
-        let difficulty = document.getElementById("difficulty").value;
-        let priority = document.getElementById("priority").value;
-        tasks.push(new Task(name, date, difficulty, priority));
-        let storagedTasks = JSON.stringify(tasks);
-        localStorage.setItem("userTasks", storagedTasks)
+        document.getElementById("taskList").innerHTML = `
+        <div class="card noTaskCard mx-auto">Aún no se han añadido tareas</div>
+        `;
     } else {
         tasksPrinter(storagedTasks);
         document.getElementById("taskCounter").innerHTML = storagedTasks.length;
@@ -105,62 +97,61 @@ let checkStorageTasks = () => {
 }
 checkStorageTasks()
 
-//Agregador de tareas
-let addTask = () => {
+//Adding tasks
+const addTask = () => {
     let tasks = JSON.parse(localStorage.getItem("userTasks"));
-    console.log(tasks)
     let name = document.getElementById("name").value;
-    let date = document.getElementById("date").value;
     let difficulty = document.getElementById("difficulty").value;
     let priority = document.getElementById("priority").value;
-    tasks.push(new Task(name, date, difficulty, priority));
+    if (tasks == null) {
+        tasks = [{ name: name, difficulty: difficulty, priority: priority }]
+    } else {
+        tasks.push(new Task(name, difficulty, priority));
+    }
     tasks.sort(function (a, b) {
         return (b.priority - a.priority)
     });
     tasksPrinter(tasks);
     let storagedTasks = JSON.stringify(tasks);
     localStorage.setItem("userTasks", storagedTasks);
-    console.log(localStorage.getItem("userTasks"))
     document.getElementById("taskCounter").innerHTML = tasks.length;
 }
 
-
-//Borrador de tareas
-let deleteTask = (deleted) => {
-    let storagedTasks = JSON.parse(localStorage.getItem("userTasks"))
-    const index = storagedTasks.findIndex(task => task.name === deleted)
-    console.log(index)
+//Deleting tasks
+const deleteTask = (deleted) => {
+    let tasks = JSON.parse(localStorage.getItem("userTasks"));
+    const index = tasks.findIndex(task => task.name === deleted);
     if (index > -1) {
-        storagedTasks.splice(index, 1);
-    }
-    console.log(storagedTasks);
+        tasks.splice(index, 1);
+    };
     const deletedTask = document.getElementById(deleted);
     deletedTask.parentNode.removeChild(deletedTask);
-    $('#taskCounter').html(storagedTasks.length);
+    $('#taskCounter').html(tasks.length);
+    let storagedTasks = JSON.stringify(tasks);
     localStorage.setItem("userTasks", storagedTasks);
-    console.log(localStorage.getItem("userTasks"))
 }
 
 //TODO: agregar botón de limpiar formulario de tarea nueva. 
 //TODO: cambiar número de prioridad por un botón que suba o baje la tarea en el listado, este botón va a cambiar la posición de la tarea en el array de tareas.
 
-
-//Buscador de tareas
-let searchTerm = () => {
+//Task finder
+const searchTerm = () => {
+    let tasks = JSON.parse(localStorage.getItem("userTasks"));
     let term = $('#search').val();
-    console.log(term)
-    const foundTask = tasks.filter(task => task.name == term);
-    console.log(foundTask)
-    tasksPrinter(foundTask);
+    if (term == "") {
+        tasksPrinter(tasks);
+    } else {
+        const foundTask = tasks.filter(task => task.name == term);
+        tasksPrinter(foundTask);
+    }
     //TODO: hacer que el buscador encuentre resultados similares
-    //TODO: hacer que al borrar la búsqueda se vuelvan a mostrar todas las tareas 
 }
 
-//Efectos con jQuery
+//jQuery effects
 $("#userArea").slideDown(1000)
 $("#phrase").fadeToggle(1000)
 
-//Utilizando API para obtener fecha según IP de usuario
+//Displaying date using API
 $.getJSON("http://worldtimeapi.org/api/ip", function (res) {
     let unix_timestamp = res.unixtime;
     let a = new Date(unix_timestamp * 1000);
@@ -170,4 +161,52 @@ $.getJSON("http://worldtimeapi.org/api/ip", function (res) {
     let date = a.getDate();
     let time = `${date} de ${month} de ${year}`;
     document.getElementById("date").innerHTML = time
-})
+});
+
+//Filters
+const filterDifficulty = () => {
+    let tasks = JSON.parse(localStorage.getItem("userTasks"));
+    let option = document.getElementById("difficultyFilter").value;
+    switch (option) {
+        case "0": 
+            tasksPrinter(tasks)
+            break;
+        case "1": 
+            let easyTasks = tasks.filter(task => task.difficulty == "Fácil");
+            tasksPrinter(easyTasks)
+            break;
+        case "2":
+            let normalTasks = tasks.filter(task => task.difficulty == "Normal");
+            tasksPrinter(normalTasks)
+            break;
+        case "3":
+            let hardTasks = tasks.filter(task => task.difficulty == "Difícil");
+            tasksPrinter(hardTasks)
+            break;
+    }
+    
+}
+
+//Displaying information
+const showInfo = () => {
+    document.getElementById("information").innerHTML = `
+    <div id="info"class="container">
+        <h3>¡Gracias por utilizar MisTareas!</h3>
+        <p>MisTareas es una aplicación web desarrollada como proyecto final.</p>
+        <div>Podés ver el codigo libremente en <a href="https://github.com/gabolope/javascript-coderhouse">GitHub</a></div>
+        <p>Seguime en mis redes:</p>
+        <div class="container row">
+            <div class="col-2 mx-auto">
+                <a href="https://github.com/gabolope/"><img src="media/icons/github.svg" alt="GitHub"></a>
+            </div>
+            <div class="col-2 mx-auto">
+                <a href="https://www.linkedin.com/in/gabriel-alejandro-l%C3%B3pez-14190720a/"><img src="media/icons/linkedin.svg" alt="Linkedin"></a>
+            </div>
+        </div>
+        <a href="#!"><img id="close" onclick="closeInfo()" src="media/icons/x.svg" alt="Close"></a>
+    </div>
+    `
+}
+const closeInfo = () => {
+    document.getElementById("information").innerHTML = ``;
+}
